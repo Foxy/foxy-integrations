ed6172b27714668762ae4706faee7649b12fc58e
Lauren Xie <lrnxie@gmail.com>
2025-04-16 17:37:52 -0400

    feat(foxy-godaddy): add support for buy now button

--- foxy-godaddy.js
+++ foxy-godaddy.js
@@ -4,14 +4,10 @@ export default function init(foxyConfig) {
       const gdAddBtn = parent.document.querySelector(
         '[data-aid="ADD_TO_CART"]'
       );
-      if (gdAddBtn && !gdAddBtn.dataset.listenerAttached) {
-        gdAddBtn.addEventListener('click', (e) => {
-          e.preventDefault();
-          e.stopImmediatePropagation();
-          foxyAddToCart(foxyConfig);
-        });
-        gdAddBtn.dataset.listenerAttached = 'true';
-      }
+      const gdBuyNowBtn = parent.document.querySelector('[data-aid="BUY_NOW"]');
+
+      foxyAddToCart(gdAddBtn);
+      foxyAddToCart(gdBuyNowBtn, true);
     }
   });
 
@@ -19,33 +15,47 @@ export default function init(foxyConfig) {
     childList: true,
     subtree: true,
   });
-}
 
-function foxyAddToCart(foxyConfig) {
-  const name = parent.document.querySelector(
-    '[data-aid="PRODUCT_NAME_RENDERED"]'
-  )?.textContent;
-  if (!name) {
-    console.error('Foxy: Cannot find product name');
-    return;
+  function foxyAddToCart(btn, checkout = false) {
+    if (btn && !btn.dataset.listenerAttached) {
+      btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+
+        const name = parent.document.querySelector(
+          '[data-aid="PRODUCT_NAME_RENDERED"]'
+        )?.textContent;
+        if (!name) {
+          console.error('Foxy: Cannot find product name');
+          return;
+        }
+
+        const price = parent.document
+          .querySelector('[data-aid="PRODUCT_PRICE_RENDERED"]')
+          ?.textContent.replace(/[^0-9.]/g, '');
+        if (!price) {
+          console.error('Foxy: Cannot find product price');
+          return;
+        }
+
+        const quantity =
+          parent.document.querySelector('[data-aid="PRODUCT_QUANTITY"]')
+            ?.value || 1;
+        const image =
+          parent.document.querySelector('#ols-image-wrapper img')?.src || '';
+
+        const url = `https://${
+          foxyConfig.storeDomain
+        }/cart?name=${encodeURIComponent(
+          name
+        )}&price=${price}&quantity=${quantity}&image=${encodeURIComponent(
+          image
+        )}${checkout ? '&cart=checkout' : ''}`;
+
+        parent.window.location.href = url;
+      });
+
+      btn.dataset.listenerAttached = 'true';
+    }
   }
-
-  const price = parent.document
-    .querySelector('[data-aid="PRODUCT_PRICE_RENDERED"]')
-    ?.textContent.replace(/[^0-9.]/g, '');
-  if (!price) {
-    console.error('Foxy: Cannot find product price');
-    return;
-  }
-
-  const quantity =
-    parent.document.querySelector('[data-aid="PRODUCT_QUANTITY"]')?.value || 1;
-  const image =
-    parent.document.querySelector('#ols-image-wrapper img')?.src || '';
-
-  const url = `https://${foxyConfig.storeDomain}/cart?name=${encodeURIComponent(
-    name
-  )}&price=${price}&quantity=${quantity}&image=${encodeURIComponent(image)}`;
-
-  parent.window.location.href = url;
 }
