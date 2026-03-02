d17aa4e67c35d707ea8549589b13fd63cb696d8e
Lauren Xie <lrnxie@gmail.com>
2026-03-02 14:58:01 -0500

    refactor(foxy-squarespace): simplify logic for variants

--- foxy-squarespace.js
+++ foxy-squarespace.js
@@ -25,7 +25,10 @@ FC.onLoad = function () {
         const name =
           document.querySelector('.ProductItem-details-title, .product-title')
             ?.textContent || Static.SQUARESPACE_CONTEXT.item.title;
-        if (!name) console.error('Foxy: cannot find product name');
+        if (!name) {
+          console.error('Foxy: cannot find product name');
+          return;
+        }
 
         const quantity =
           document.querySelector('.product-quantity-input input')?.value ||
@@ -51,112 +54,62 @@ FC.onLoad = function () {
         // get variant info from static Squarespace context
         const variantData = Static.SQUARESPACE_CONTEXT.product.variants;
 
-        // select elements for variants
-        const variantSelect = document.querySelectorAll(
-          '.variant-select-wrapper select'
+        const optionEls = document.querySelectorAll(
+          '#main-product-variants .variant-option'
         );
 
-        if (variantSelect.length > 0) {
-          // product has variants
-          // get selected variant name and value from elements
-          const allVariantName = Array.from(variantSelect).map(
-            (variant) =>
-              variant.dataset.variantOptionName ||
-              variant.parentElement.parentElement.dataset.variantOptionName
-          );
-          const allVariantValue = Array.from(variantSelect).map(
-            (variant) => variant.value
-          );
+        const options = {};
+        for (const optionEl of optionEls) {
+          const selectEl = optionEl.querySelector('.variant-select');
+          const optionName =
+            optionEl.dataset.variantOptionName ||
+            selectEl.dataset.variantOptionName;
+          const optionValue = selectEl.value;
 
-          // if all variants have valid selected values
-          if (!allVariantValue.includes('')) {
-            // concat variant name and value
-            let variantParam = '';
-
-            allVariantName.forEach(
-              (name, index) =>
-                (variantParam += `&${encodeURIComponent(
-                  name
-                )}=${encodeURIComponent(allVariantValue[index])}`)
-            );
-
-            // find the selected variant info from variant data
-            let selectedVariant;
-
-            if (allVariantName.length === 1) {
-              selectedVariant = variantData.find(
-                (variant) =>
-                  variant.attributes[allVariantName[0]] === allVariantValue[0]
-              );
-            } else if (allVariantName.length === 2) {
-              selectedVariant = variantData.find(
-                (variant) =>
-                  variant.attributes[allVariantName[0]] ===
-                    allVariantValue[0] &&
-                  variant.attributes[allVariantName[1]] === allVariantValue[1]
-              );
-            } else if (allVariantName.length === 3) {
-              selectedVariant = variantData.find(
-                (variant) =>
-                  variant.attributes[allVariantName[0]] ===
-                    allVariantValue[0] &&
-                  variant.attributes[allVariantName[1]] ===
-                    allVariantValue[1] &&
-                  variant.attributes[allVariantName[2]] === allVariantValue[2]
-              );
-            }
-
-            // get product info from variant data
-            const price = selectedVariant.onSale
-              ? selectedVariant.salePrice.decimalValue
-              : selectedVariant.price.decimalValue;
-            const stock = selectedVariant.stock.quantity || '';
-            const { sku, id: variantId } = selectedVariant;
-            const weight = selectedVariant.shippingWeight?.value ?? '';
-            const {
-              height = '',
-              len = '',
-              width = '',
-            } = selectedVariant.shippingSize ?? {};
-
-            // trigger Foxy cart-submit event only if product is in stock
-            if (stock !== 0) {
-              FC.client.event('cart-submit').trigger({
-                data: { cart: 'add' },
-                url:
-                  cartUrl +
-                  variantParam +
-                  `&price=${price}&quantity_max=${stock}&code=${encodeURIComponent(
-                    sku
-                  )}&weight=${weight}&height=${height}&length=${len}&width=${width}&variant_id=${variantId}`,
-              });
-            }
+          if (!optionName || !optionValue) {
+            console.error('Foxy: invalid option name or value');
+            return;
           }
+
+          options[optionName] = optionValue;
+        }
+
+        const selectedVariant =
+          variantData.length === 1
+            ? variantData[0]
+            : variantData.find((variant) =>
+                Object.entries(options).every(
+                  ([key, value]) => variant.attributes[key] === value
+                )
+              );
+
+        if (!selectedVariant) {
+          console.error('Foxy: cannot find a matching selected variant');
+          return;
+        }
+
+        const price = selectedVariant.onSale
+          ? selectedVariant.salePrice.decimalValue
+          : selectedVariant.price.decimalValue;
+        const stock = selectedVariant.stock.quantity || '';
+        const { sku, id: variantId } = selectedVariant;
+        const weight = selectedVariant.shippingWeight?.value ?? '';
+        const {
+          height = '',
+          len = '',
+          width = '',
+        } = selectedVariant.shippingSize ?? {};
+
+        if (stock !== 0) {
+          FC.client.event('cart-submit').trigger({
+            data: { cart: 'add' },
+            url: `${cartUrl}&price=${price}&quantity_max=${stock}&code=${encodeURIComponent(
+              sku
+            )}&weight=${weight}&height=${height}&length=${len}&width=${width}&variant_id=${variantId}&${new URLSearchParams(Object.entries(options)).toString()}`,
+          });
         } else {
-          // product has no variants
-          const price = variantData[0].onSale
-            ? variantData[0].salePrice.decimalValue
-            : variantData[0].price.decimalValue;
-          const stock = variantData[0].stock.quantity || '';
-          const { sku, id: variantId } = variantData[0];
-          const weight = variantData[0].shippingWeight?.value ?? '';
-          const {
-            height = '',
-            len = '',
-            width = '',
-          } = variantData[0].shippingSize ?? {};
-
-          // trigger Foxy cart-submit event only if product is in stock
-          if (stock !== 0) {
-            FC.client.event('cart-submit').trigger({
-              data: { cart: 'add' },
-              url:
-                cartUrl +
-                `&price=${price}&quantity_max=${stock}&code=${encodeURIComponent(
-                  sku
-                )}&weight=${weight}&height=${height}&length=${len}&width=${width}&variant_id=${variantId}`,
-            });
-          }
+          console.error('Foxy: no stock available');
+          return;
         }
       });
     });
